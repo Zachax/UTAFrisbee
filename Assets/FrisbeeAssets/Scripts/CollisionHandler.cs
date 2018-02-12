918b76ebe32dbc9943ea4fec5c87411da753fe4c
Tuukka Tilsa <tuukka.tilsa@gmail.com>
2018-02-12 21:01:50 +0200

    Target hit detection.
    Frisbee can hit and destroy target objects. New replay camera.

---
+++ Assets/FrisbeeAssets/Scripts/CollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Frisbee interactions with other objects. Main purpose is to determine if a throw hit
+ * the target.
+ * 
+ * 
+ * 
+ */
+
+public class CollisionHandler : MonoBehaviour {
+
+    // Checks for collision event (ie. hitting something)
+    void OnCollisionEnter(Collision collisionDetails) {
+       // Debug.Log("Frisbee hit something" + collisionDetails.collider);
+
+        if (collisionDetails.gameObject.name == "Target")
+        {
+            Destroy(collisionDetails.gameObject);
+            print("HIT!");
+        }
+    
+
+    }
+    
+
+}
